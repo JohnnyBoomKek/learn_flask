a46ec6f1f861ec27b9f9818c1a472918af3585c0
JohnnyBoomKek <summer1710@qq.com>
2021-03-24 10:47:57 +0800

    forms

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from flask import Flask
+from flask import request, Flask, render_template
+from flask.helpers import make_response
+from flask_bootstrap import Bootstrap 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard_to_guess'
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>HelloWorld</h1>'
-app.add_url_rule('/', 'index', index)
+    user_agent = request.headers.get('User-Agent')
+    return render_template('index.html')
 
-@app.route('/users/<name>')
-def user(name):
-    return f'hello, {name}'
+@app.route('/cookie')
+def cookie():
+    response = make_response('<h1> this document carries a cookie</h1>')
+    response.set_cookie('answer', '42')
+    return response
